e4ecdc25969425a1469023baabe82d6b058ad970
ltlylfun <funjee126@qq.com>
2025-05-22 14:21:12 +0800

    修改

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,12 +64,12 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/logo.jpg",
     navbar: {
       title: "ltlyl-web",
       logo: {
         alt: "ltlyl-web Logo",
-        src: "img/logo.svg",
+        src: "img/logo.jpg",
       },
       items: [
         {
